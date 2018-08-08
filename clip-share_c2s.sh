a419623eac521adebfefcd35027b4a3dadfb0965
umaumax <umauma7083@gmail.com>
2018-08-08 21:02:30 +0900

    Delete extra files

--- clip-share_c2s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-server=$1
-
-export LC_CODE=$(cat s2c.sh | base64)
-# macをserver側にする場合にはbase64 -Dとなる
-./c2s.sh | command ssh "$server" "echo $LC_CODE | base64 -d > .clip-share-tmp.sh; bash .clip-share-tmp.sh"
-
--- clip-share_s2c.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-server=$1
-cat c2s.sh | command ssh "$server" "bash -s" | ./s2c.sh
-
